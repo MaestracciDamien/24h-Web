75f0ef78b51d95fca4fc680f7a58e2e9c0e1985a
Bailed <besson.maxime@gmail.com>
2015-05-29 23:21:23 +0200

    Suppression vue

--- includes/footer.php
+++
@@ -1,3 +0,0 @@
-<nav class="navbar navbar-default navbar-fixed-bottom">
-  <p class="navbar-text navbar-right">© WeLeakedGOT</p>
-</nav>
--- includes/script.php
+++
@@ -1,7 +0,0 @@
-<script src="js/libs/jquery/dist/jquery.min.js"></script>
-
-<script src="js/libs/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="js/nlform.js"></script>
-<script type="text/javascript">
-	var nlform = new NLForm( document.getElementById( 'nl-form' ) );	
-</script>
