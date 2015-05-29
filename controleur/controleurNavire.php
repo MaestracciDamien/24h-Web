6838c7fc8f4533e6b664edbed9c7fb7248b3bbc1
MaestracciDamien <damien.maestracci@etu.univ-nantes.fr>
2015-05-30 01:05:25 +0200

    contrôleur navire

---
+++ controleur/controleurNavire.php
@@ -0,0 +1,17 @@
+<?php
+class ControleurNavire{
+ 
+  private $modele;
+  private $vue;
+ 
+	public function __construct(){
+		$this->modele=new Dao();
+		$this->vue = new Vue();
+	}
+
+	public function afficherNavire($id){
+		$this->vue->genereVueNavire($this->modele->getNavire($id));
+	}
+
+}
+?>
