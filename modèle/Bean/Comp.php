480688e3ceaab0cb670cf5e5c34347e1e4f7cc8e
Ouisticram <quentin.auger-dubois@outlook.com>
2015-05-29 23:23:16 +0200

    bean on the road

---
+++ modèle/Bean/Comp.php
@@ -0,0 +1,23 @@
+<?php
+
+class Comp{
+
+	private $id;
+	private $nom;
+	private $adresse;
+	private $pays;
+
+	// Constructeur de la classe
+	public function __construct($i, $n, $a, $p){
+		$this->id = $i;
+		$this->nom = $n;
+		$this->adresse = $a;
+		$this->pays = $p;
+	}
+
+	public function getId(){ return $this->id; }
+	public function getNom(){ return $this->nom; }
+	public function getAdresse(){ return $this->adresse; }
+	public function getPays(){ return $this->payss; }
+	
+}
