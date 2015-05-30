3744fea38dd3b644d525c4bb32272774ccce3292
Bailed <besson.maxime@gmail.com>
2015-05-30 03:09:59 +0200

    Mise à jour index selon les 3

--- vues/affichage.php
+++ vues/affichage.php
@@ -12,34 +12,50 @@ require_once 'includes/nav.php';
 
 		function index()
 		{
-			include 'includes/head.php';
-			$this->nav->navbar();
-			?>
-			<div class="main clearfix">
-				<h1>Outil de gestion portuaire du Havre</h1>
-				<h2>Epreuve 2 - 24h IUT Nantes</h2>
-				<p>Bienvenue sur l'outil de gestion portuaire du Havre. Vous y trouverez votre accès client vous permettant de gérer vos navires et conteneurs.</p>
-				<h2>Première venue sur notre outil ? Connectez-vous !</h2>
-				<h3>Je suis :</h3>
-				<div class="row">
-					<div class="col-md-4 text-center">
-						<a href="?connect"><i class="fa fa-ship fa-4x"></i>
-						<h3>Compagnie</h3></a>
+			if(isset($_SESSION["pseudo"])) {
+					
+				if($_SESSION["type"] == 1) {
+					$this->index_comp();
+				}
+				elseif($_SESSION["type"] == 2) {
+					$this->index_client();
+				}
+				elseif($_SESSION["type"] == 3) {
+					$this->index_agent();
+				}	
+			}				
+
+			else {
+
+				include 'includes/head.php';
+				$this->nav->navbar();
+				?>
+				<div class="main clearfix">
+					<h1>Outil de gestion portuaire du Havre</h1>
+					<h2>Epreuve 2 - 24h IUT Nantes</h2>
+					<p>Bienvenue sur l'outil de gestion portuaire du Havre. Vous y trouverez votre accès client vous permettant de gérer vos navires et conteneurs.</p>
+					<h2>Première venue sur notre outil ? Connectez-vous !</h2>
+					<h3>Je suis :</h3>
+					<div class="row">
+						<div class="col-md-4 text-center">
+							<a href="?connect"><i class="fa fa-ship fa-4x"></i>
+							<h3>Compagnie</h3></a>
+						</div>
+						<div class="col-md-4 text-center">
+							<a href="?connect"><i class="fa fa-users fa-4x"></i>
+							<h3>Clients</h3></a>
+						</div>
+						<div class="col-md-4 text-center">
+							<a href="?connect"><i class="fa fa-shield fa-4x"></i>
+							<h3>Agent portuaire</h3></a>
+							</div>
+						</div>
 					</div>
-					<div class="col-md-4 text-center">
-						<a href="?connect"><i class="fa fa-users fa-4x"></i>
-						<h3>Clients</h3></a>
-					</div>
-					<div class="col-md-4 text-center">
-						<a href="?connect"><i class="fa fa-shield fa-4x"></i>
-						<h3>Agent portuaire</h3></a>
-					</div>
-				</div>
-			</div>
-			<?php
-			include 'includes/footer.php';
-			include 'includes/script.php';
-			include 'includes/foot.php';
+					<?php
+					include 'includes/footer.php';
+					include 'includes/script.php';
+					include 'includes/foot.php';
+			}
 		}
 
 
@@ -195,6 +211,51 @@ require_once 'includes/nav.php';
 					</div>
 					<div class="col-md-4 text-center"></div>
 				</div>
+			</div>
+			<?php
+			include 'includes/footer.php';
+			include 'includes/script.php';
+			include 'includes/foot.php';
+		}
+
+		function index_comp()
+		{
+			include 'includes/head.php';
+			$this->nav->navbar();
+			?>
+			<div class="main clearfix">
+				<h1>Votre interface de gestion Compagnie</h1>
+				
+			</div>
+			<?php
+			include 'includes/footer.php';
+			include 'includes/script.php';
+			include 'includes/foot.php';
+		}
+
+		function index_client()
+		{
+			include 'includes/head.php';
+			$this->nav->navbar();
+			?>
+			<div class="main clearfix">
+				<h1>Votre interface de gestion Client</h1>
+				
+			</div>
+			<?php
+			include 'includes/footer.php';
+			include 'includes/script.php';
+			include 'includes/foot.php';
+		}
+
+		function index_agent()
+		{
+			include 'includes/head.php';
+			$this->nav->navbar();
+			?>
+			<div class="main clearfix">
+				<h1>Votre interface de gestion Agent</h1>
+				
 			</div>
 			<?php
 			include 'includes/footer.php';
